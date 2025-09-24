fc10397959132ff6edb59a14e248a9fdd4a49374
Zolstoy <zolstoy.dev@gmail.com>
2025-09-25 01:18:36 +0200

    WIP

---
+++ BlockGateAnd.cs
@@ -0,0 +1,21 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+
+namespace LogicBlocks.Blocks
+{
+    internal class BlockGateAnd : Block
+    {
+        public override void OnBlockPlaced(IWorldAccessor world, BlockPos blockPos, ItemStack byItemStack = null)
+        {
+            api.RegisterBlockClass(Mod.Info.ModID + ".gate_and", typeof(BlockGateAnd));
+            api.Logger.Event("Block Placed!");
+            base.OnBlockPlaced(world, blockPos, byItemStack);
+        }
+
+        public override void OnBlockBroken(IWorldAccessor world, BlockPos pos, IPlayer byPlayer, float dropQuantityMultiplier = 1)
+        {
+            api.Logger.Event("Block Broken!");
+            base.OnBlockBroken(world, pos, byPlayer, dropQuantityMultiplier);
+        }
+    }
+}
