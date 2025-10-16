8a93371b6098d3a2c93fc966bfaffe1a4eaabf38
Zolstoy <zolstoy.dev@gmail.com>
2025-10-16 09:57:58 +0200

    WIP

---
+++ LogicBlocks/Items/Connector.cs
@@ -0,0 +1,18 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+
+namespace LogicBlocks.Items
+{
+    internal class Connector : Item
+    {
+        public override void OnHeldUseStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel,
+            EnumHandInteract useType, bool firstEvent, ref EnumHandHandling handling)
+        {
+            if (useType == EnumHandInteract.HeldItemInteract)
+            {
+                byEntity.Api.Logger.Event("CONNECT");
+            }
+            //base.OnHeldUseStart(slot, byEntity, blockSel, entitySel, useType, firstEvent, ref handling);
+        }
+    }
+}
--- LogicBlocks/LogicBlocksModSystem.cs
+++ LogicBlocks/LogicBlocksModSystem.cs
@@ -1,6 +1,6 @@
-﻿using Vintagestory.API.Client;
+﻿using LogicBlocks.Items;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
-using Vintagestory.API.Config;
 using Vintagestory.API.Server;
 
 namespace LogicBlocks
@@ -8,21 +8,17 @@ namespace LogicBlocks
     public class LogicBlocksModSystem : ModSystem
     {
 
-        // Called on server and client
-        // Useful for registering block/entity classes on both sides
         public override void Start(ICoreAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod: " + api.Side);
+            api.RegisterItemClass(Mod.Info.ModID + ".connector", typeof(Connector));
         }
 
         public override void StartServerSide(ICoreServerAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("logicblocks:hello"));
         }
 
         public override void StartClientSide(ICoreClientAPI api)
         {
-            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("logicblocks:hello"));
         }
 
     }
