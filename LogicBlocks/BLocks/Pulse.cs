0ce9c829d263cf935a85f4b3cf9f85e4e3b6082b
Zolstoy <zolstoy.dev@gmail.com>
2025-10-16 23:01:18 +0200

    WIP

--- LogicBlocks/BLocks/Pulse.cs
+++ LogicBlocks/BLocks/Pulse.cs
@@ -1,17 +1,45 @@
 ﻿using System.Collections.Generic;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
 using Vintagestory.API.Util;
 
 namespace LogicBlocks.BLocks
 {
 
-    internal class Pulse: Block
+    internal class Pulse : Block
     {
+        public BlockPos position;
         public List<Block> connected_blocks;
 
-        public Pulse() { 
-            connected_blocks = new List<Block>();
+        public Pulse()
+        {
+            position = new BlockPos(0, 0, 0);
+            connected_blocks = [];
+        }
+
+        public override void OnBlockPlaced(IWorldAccessor world, BlockPos blockPos, ItemStack byItemStack = null)
+        {
+            position = blockPos.Copy();
+            base.OnBlockPlaced(world, blockPos, byItemStack);
+        }
+
+        public override void OnBeforeRender(ICoreClientAPI capi, ItemStack itemstack, EnumItemRenderTarget target, ref ItemRenderInfo renderinfo)
+        {
+            foreach (Block block in connected_blocks) {
+
+                var pulse = block as Pulse;
+                var lineMesh = new MeshData(2, 0, false, false, false, false);
+
+                lineMesh.AddVertex(position.X, position.Y, position.Z, 1, 1);
+                lineMesh.AddVertex(pulse.position.X, pulse.position.Y, pulse.position.Z, 1, 1);
+
+                var mesh_ref = capi.Render.UploadMesh(lineMesh);
+
+                capi.Render.RenderMesh(mesh_ref);
+                base.OnBeforeRender(capi, itemstack, target, ref renderinfo);
+            }
         }
     }
 }
--- LogicBlocks/Items/Connector.cs
+++ LogicBlocks/Items/Connector.cs
@@ -9,40 +9,32 @@ namespace LogicBlocks.Items
     internal class Connector : Item
     {
         Block? first_block;
-        //Block? second_block;
 
         public override void OnHeldUseStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel,
             EnumHandInteract useType, bool firstEvent, ref EnumHandHandling handling)
         {
-            if (useType == EnumHandInteract.HeldItemInteract && blockSel != null)
+            if (useType != EnumHandInteract.HeldItemInteract || blockSel == null || Regex.Count(blockSel.Block.Code.ToString(), "^logicblocks:.*") == 0)
+                return;
+            var system = byEntity.Api.ModLoader.GetModSystem<LogicBlocksModSystem>();
+            if (system == null)
             {
-                if (Regex.Count(blockSel.Block.Code.ToString(), "^logicblocks:.*") > 0)
-                {
+                byEntity.Api.Logger.Event("CRITICAL: NO SYSTEM");
+                return;
+            }
 
-                    var system = byEntity.Api.ModLoader.GetModSystem<LogicBlocksModSystem>();
-                    if (system == null)
-                    {
-                        byEntity.Api.Logger.Event("CRITICAL: NO SYSTEM");
-                        return;
-                    }
-
-                    if (first_block != null)
-                    {
-                        byEntity.Api.Logger.Event("SECOND CONNECT");
-                        (first_block as Pulse).connected_blocks.Add(blockSel.Block);
-                        first_block = null;
-                    }
-                    else
-                    {
-                        byEntity.Api.Logger.Event("FIRST CONNECT");
-                        first_block = blockSel.Block;
-                    }
-                }
-
+            if (first_block != null)
+            {
+                byEntity.Api.Logger.Event("SECOND CONNECT");
+                (first_block as Pulse).connected_blocks.Add(blockSel.Block);
+                first_block = null;
+            }
+            else
+            {
+                byEntity.Api.Logger.Event("FIRST CONNECT");
+                first_block = blockSel.Block;
             }
         }
 
-        //base.OnHeldUseStart(slot, byEntity, blockSel, entitySel, useType, firstEvent, ref handling);
     }
 }
 
